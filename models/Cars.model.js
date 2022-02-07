5c46eac41359d12e6f4cb9b72ce8cd0c8674afe2
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-02-07 16:39:31 +0100

    Models modified

---
+++ models/Cars.model.js
@@ -0,0 +1,22 @@
+// models/Cars.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+  make: String,  
+  model: String,
+  yearOfProd: Number,
+  engine: String,
+  transmission: String,
+  fuel: String,
+  extras: String,
+  rentalCost: Number,
+  status: Boolean,
+  startDate: Date,
+  endDate: Date,
+});
+
+const Cars = mongoose.model('Cars', carSchema);
+
+module.exports = Cars;
